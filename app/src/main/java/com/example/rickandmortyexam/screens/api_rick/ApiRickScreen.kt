cb9337431fe06940a78e6026c8c4bde8a7ddf618
AndiCoding <andi98@live.no>
2024-11-19 21:06:31 +0100

    moved buttons to reusable component

--- app/src/main/java/com/example/rickandmortyexam/screens/api_rick/ApiRickScreen.kt
+++ app/src/main/java/com/example/rickandmortyexam/screens/api_rick/ApiRickScreen.kt
@@ -17,6 +17,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.text.TextStyle
 import com.example.rickandmortyexam.components.CharacterItem
+import com.example.rickandmortyexam.components.PrevNextButtons
 import com.example.rickandmortyexam.ui.theme.Typography
 
 
@@ -38,27 +39,6 @@ fun ApiRickScreen(apiRickViewModel: ApiRickViewModel) {
             }
         }
 
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            Arrangement.SpaceEvenly
-        ) {
-            Button(
-                modifier = Modifier.weight(1f),
-                shape = RectangleShape,
-                onClick = { apiRickViewModel.prevCharacters() }
-
-
-            ) {
-                Text("Prev")
-            }
-            Spacer(modifier = Modifier.weight(0.1f))
-            Button(
-                modifier = Modifier.weight(1f),
-                shape = RectangleShape,
-                onClick = {apiRickViewModel.nextCharacters()}
-            ) {
-                Text("Next")
-            }
-        }
+        PrevNextButtons(apiRickViewModel::prevCharacters, apiRickViewModel::nextCharacters)
     }
 }
