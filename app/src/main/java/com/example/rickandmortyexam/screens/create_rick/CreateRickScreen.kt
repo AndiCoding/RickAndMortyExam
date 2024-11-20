2406655333491c98b822537715068487deea7907
AndiCoding <andi98@live.no>
2024-11-20 13:21:42 +0100

    Update CreateRickScreen.kt

--- app/src/main/java/com/example/rickandmortyexam/screens/create_rick/CreateRickScreen.kt
+++ app/src/main/java/com/example/rickandmortyexam/screens/create_rick/CreateRickScreen.kt
@@ -1,13 +1,14 @@
 package com.example.rickandmortyexam.screens.create_rick
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -15,6 +16,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -39,34 +41,47 @@ fun CreateRickScreen(createRickViewModel: CreateRickViewModel) {
         }
     }
 
-    Column(Modifier.fillMaxSize())
+    Column(
+        Modifier.fillMaxSize(),
+        Arrangement.SpaceEvenly,
+        horizontalAlignment =
+            Alignment.CenterHorizontally
+    )
     {
         Text("Create Character", style = Typography.headlineLarge)
-        TextField(
-            value = createdCharacterScreen.name,
-            onValueChange = { createRickViewModel.updateCharacterField("name", it) },
-            label = { Text("Name") }
-        )
-        TextField(
-            value = createdCharacterScreen.status,
-            onValueChange = { createRickViewModel.updateCharacterField("status", it) },
-            label = { Text("Status") }
-        )
-        TextField(
-            value = createdCharacterScreen.species,
-            onValueChange = {createRickViewModel.updateCharacterField("species", it) },
-            label = { Text("Species") }
-        )
-        TextField(
-            value = createdCharacterScreen.type,
-            onValueChange = { createRickViewModel.updateCharacterField("type", it) },
-            label = { Text("Type") }
-        )
-        TextField(
-            value = createdCharacterScreen.gender,
-            onValueChange = { createRickViewModel.updateCharacterField("gender", it) },
-            label = { Text("Gender") }
-        )
+        Column {
+            TextField(
+                value = createdCharacterScreen.name,
+                onValueChange = { createRickViewModel.updateCharacterField("name", it) },
+                label = { Text("Name") }
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            TextField(
+                value = createdCharacterScreen.status,
+                onValueChange = { createRickViewModel.updateCharacterField("status", it) },
+                label = { Text("Status") }
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            TextField(
+                value = createdCharacterScreen.species,
+                onValueChange = {createRickViewModel.updateCharacterField("species", it) },
+                label = { Text("Species") }
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            TextField(
+                value = createdCharacterScreen.type,
+                onValueChange = { createRickViewModel.updateCharacterField("type", it) },
+                label = { Text("Type") }
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            TextField(
+                value = createdCharacterScreen.gender,
+                onValueChange = { createRickViewModel.updateCharacterField("gender", it) },
+                label = { Text("Gender") }
+            )
+
+        }
+
 
         Button(onClick = {
             createRickViewModel.insertCharacter()
@@ -75,18 +90,18 @@ fun CreateRickScreen(createRickViewModel: CreateRickViewModel) {
         }
 
 
-        // displays a message if the character was created successfully or not
-        message?.let {
-            Box(
-                modifier = Modifier
-                    .height(50.dp)
-                    .width(200.dp)
-                    .background(if (it.contains("successfully")) Color(0xFF4BB543) else Color(0xFFFF3333))
-                    .padding(16.dp)
-            ) {
-                Text(it, color = Color.White)
-            }
+    }
+    // displays a message if the character was created successfully or not
+    message?.let {
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier
+                .height(50.dp)
+                .fillMaxWidth()
+                .background(if (it.contains("successfully")) Color(0xFF4BB543) else Color(0xFFFF3333))
+                .padding(16.dp)
+        ) {
+            Text(it, color = Color.White)
         }
-
     }
 }
