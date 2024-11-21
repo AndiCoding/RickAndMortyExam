1bdbb4a5d4fdd479215c8cbb20149c2e7eb2d9c7
monicarito15 <carolinamera1985@gmail.com>
2024-11-21 11:18:12 +0100

    update showRickScreen

--- app/src/main/java/com/example/rickandmortyexam/screens/show_rick/ShowRickScreen.kt
+++ app/src/main/java/com/example/rickandmortyexam/screens/show_rick/ShowRickScreen.kt
@@ -1,8 +1,13 @@
 package com.example.rickandmortyexam.screens.show_rick
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
@@ -11,6 +16,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import com.example.rickandmortyexam.ui.theme.Typography
 
 @Composable
@@ -27,11 +34,33 @@ fun ShowRickScreen(showRickViewModel: ShowRickViewModel) {
         Button(onClick = {
             showRickViewModel.setCharacter()
         }) {
-            Text("Get all characters")
+            Text("Reload characters")
         }
-        LazyColumn {
+        LazyColumn (
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ){
             items(characters.value) { character ->
-                Text(text = character.name)
+
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp)
+                        .border(1.dp, Color.LightGray),
+
+
+                ) {
+                    Column(
+                        modifier = Modifier.padding(16.dp)
+                    ){
+                        Text(text = "Name: ${character.name}", style = Typography.headlineSmall)
+                        Text(text = "Status: ${character.status}", style = Typography.headlineSmall)
+                        Text(text = "Species: ${character.species}", style = Typography.headlineSmall)
+                        Text(text = "Type: ${character.type}", style = Typography.headlineSmall)
+                        Text(text = "Gender: ${character.gender}", style = Typography.headlineSmall)
+                    }
+
+                }
             }
         }
     }
